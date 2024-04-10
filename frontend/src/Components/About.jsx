4c624f8dfc8bd71d6ab8aeafdff0934622dad858
Chintan <patelchintan18018@gmail.com>
2024-04-10 19:55:47 +0530

    first commit

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from 'react';
+import p1_product_i2 from '../Assets/p1_product_i2.png'
 
 function About() {
   return (
-    <div>About</div>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold font-head mb-4">About Our Clothing Store</h1>
+      <div className="flex flex-col md:flex-row items-stretch mt-16">
+        {/* <div className="w-full md:w-1/2 flex items-center">
+          <img
+            className="w-full h-auto rounded-lg shadow-md mb-4"
+            src={p1_product_i2}
+            alt="Clothing Store"
+          />
+        </div> */}
+        <div className="w-full ">
+          <p className="text-lg mb-4">
+            Welcome to our clothing store! We are passionate about providing
+            high-quality, stylish clothing for all occasions. Our mission is to
+            make you look and feel great every day.
+          </p>
+          <p className="text-lg mb-4">
+            Our collection features a wide range of clothing items, from casual
+            wear to formal attire. Whether you're looking for the perfect
+            outfit for a night out or comfortable loungewear for a cozy day at
+            home, we've got you covered.
+          </p>
+          <p className="text-lg mb-4">
+            At our store, we believe in offering not only great products but
+            also exceptional customer service. Our team is dedicated to helping
+            you find the perfect pieces to express your unique style.
+          </p>
+          <p className="text-lg">
+            Thank you for choosing our clothing store. We look forward to
+            serving you and helping you create your perfect wardrobe!
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default About
+export default About;
--- frontend/src/Components/Office.jsx
+++ frontend/src/Components/Office.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 function Office() {
   return (
-    <div>Office</div>
-  )
+    <div className="office my-5">
+      <div className="shop-details flex item-center justify-around gap-5 my-3">
+        <div className="shop-address flex flex-col sm:flex-row items-center gap-5">
+          <div className="">
+            <i class="fa-solid fa-location-dot text-2xl text-red-500"></i>
+          </div>
+          <div>
+            <h1>Viviana Mall</h1>
+            <h1>Thane West, Maharashtra </h1>
+          </div>
+        </div>
+        <div className="shop-mobile flex flex-col sm:flex-row items-center gap-5">
+          <div className="">
+            <i class="fa-solid fa-phone-volume text-2xl text-red-500"></i>
+          </div>
+          <div>+91-XXXXXXXXXX</div>
+        </div>
+      </div>
+      <div className="shop-location my-3 p-10">
+        <iframe
+          className="w-full h-96"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3767.6919150453227!2d72.9690864749809!3d19.208654082023397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b97304d636f1%3A0xd7d62dc2bdd3f9cc!2sViviana%20Mall!5e0!3m2!1sen!2sin!4v1712753324389!5m2!1sen!2sin"
+          allowFullscreen=""
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    </div>
+  );
 }
 
-export default Office
+export default Office;
